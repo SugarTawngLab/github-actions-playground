9aefb1ed1d9062b6be412d20e246e99e60e33ceb
AzureAD\DienTang <tangvietdien0707@gmail.com>
2025-12-27 04:19:55 +0700

    merge

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Hello, World!");
 console.log("Add **.js again effects in yml files!");
 console.log(" main branch added2 to test negation!........");
-console.log("do not build added to test required-labels action!!");
+console.log("do not build added to test required-labels action!!!");
